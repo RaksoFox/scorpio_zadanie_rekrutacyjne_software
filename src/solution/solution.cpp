4aca4ac88b98f59aef532115051ad91ddac5c3bf
RaksoFox <raksofox@vivaldi.net>
2025-10-02 20:59:30 +0200

    chore: idk it works???

--- src/solution/solution.cpp
+++ src/solution/solution.cpp
@@ -15,24 +15,6 @@ MotorDriver yAxisDriver(680.0, 0.4);
 
 int solver(std::shared_ptr<backend_interface::Tester> tester, bool preempt)
 {
-  /* Short example you can remove it
-  std::cout << (preempt ? "Preempt" : "Queue") << '\n';
-  auto motor1 = tester->get_motor_1();
-  auto motor2 = tester->get_motor_2();
-  auto commands = tester->get_commands();
-  motor1->add_data_callback([](const uint16_t& data) {
-    std::cout << "Motor 1 data: " << static_cast<int>(data) << "\n";
-  });
-  motor2->add_data_callback([](const uint16_t& data) {
-    std::cout << "Motor 2 data: " << static_cast<int>(data) << "\n";
-  });
-  commands->add_data_callback([](const Point& point) {
-    std::cout << "Command point: (" << point.x << ", " << point.y << ", " << point.z << ")\n";
-  });
-  motor1->send_data(100);
-  motor2->send_data(-50);
-  std::this_thread::sleep_for(std::chrono::milliseconds(250));
-  */
 
   auto motor1 = tester->get_motor_1();
   auto motor2 = tester->get_motor_2();
@@ -55,8 +37,8 @@ int solver(std::shared_ptr<backend_interface::Tester> tester, bool preempt)
     targetAngles.store(point_to_angle(point));
   });
 
-  targetAngles.store(point_to_angle(Point{1, -1, 1}));
-
+  // Tests
+  // targetAngles.store(point_to_angle(Point{1, -1, 1}));
   // std::cout<<point_to_angle(Point{1, 1, 0}).z*180/PI << std::endl;
   // std::cout<<point_to_angle(Point{1, -1, 0}).z*180/PI << std::endl;
   // std::cout<<point_to_angle(Point{-1, 1, 0}).z*180/PI << std::endl;
@@ -74,10 +56,10 @@ int solver(std::shared_ptr<backend_interface::Tester> tester, bool preempt)
     prevTime = currTime;
     double dt = elapsed.count();
 
-    mtr.z = zAxisDriver.compute(target.z, position.z, dt);
+    mtr.z = -zAxisDriver.compute(target.z, position.z, dt);
     mtr.y = yAxisDriver.compute(target.y, position.y, dt);
 
-    motor1->send_data(mtr.z);
+    motor1->send_data(-mtr.z);
     motor2->send_data(mtr.y);
 
     std::cout << "---------- DEBUG ----------" << std::endl;
